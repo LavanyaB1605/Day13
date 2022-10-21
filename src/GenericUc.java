1cea0be39eb3360421bc48b368ffe34d96baab6a
LavanyaB1605 <100491624+LavanyaB1605@users.noreply.github.com>
2022-10-21 23:36:22 +0530

    Update GenericUc.java

--- src/GenericUc.java
+++ src/GenericUc.java
@@ -13,7 +13,7 @@ public class GenericUc {
         System.out.printf("Max of Integer %d , %d , %d is %d\n\n", 5,4,3,maximum(5,4,3));
         System.out.printf("Max of Float %f , %f , %f is %f\n\n", 7.4,3.7,9.8, maximum(7.4,3.7,9.8));
         System.out.printf("Max of String %s , %s , %s is %s\n\n", "Apple","Peach","Banana",maximum("Apple","Peach","Banana"));
-        //Maximum number given at Third position
+        //Maximum number given at Second position
         System.out.printf("Max of Integer when in 2nd position %d , %d , %d is %d\n", 4,5,3,maximum(4,5,3));
         System.out.printf("Max of Float when in 2nd position %f , %f , %f is %f\n", 3.7,9.4,6.8, maximum(3.7,9.4,6.8));
         System.out.printf("Max of String when in 2nd position %s , %s , %s is %s\n\n", "Apple","Peach","Banana",maximum("Apple","Peach","Banana"));
